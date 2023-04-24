38419b84cfce705dc10cf810ead6d1b1a814ab0a
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-04-24 11:39:23 +0200

    added player module

--- src/player.js
+++ src/player.js
@@ -1,21 +1,38 @@
 //import { GameBoard } from "./gameboard";
 
-class Player {
-    constructor(kind) {
-        this.kind = kind;
+export class Player {
+    constructor(type) {
+        this.type = type;
+        this.moves = [];
     }
 
-    takeTurn(board) {
-        if (this.kind === 'computer') {
-
+    takeTurn() {
+        if (this.type === 'computer') {
+            const x = Math.floor(Math.random() * 10 + 1);
+            const y = Math.floor(Math.random() * 10 + 1);
             
-            let x = Math.floor(Math.random() * 10);
-            let y = Math.floor(Math.random() * 10);
+            if (this.moves.length >= 100) {
+                return '100 moves made';
+            }
+            else if (this.moves.reduce((val, arr) => (arr[0] === x && arr[1] === y) || val, false)) {
+                return this.takeTurn();
+            } else {
+                this.moves.push([x, y]);
+                return [x, y];
+            }
+        } else if (this.type === 'human') {
+            const x = prompt('enter x coordinate', 'x');
+            const y = prompt('enter y coordinate', 'y');
 
-
-
-        } else if (this.kind === 'human') {
-
+            if (this.moves.length >= 100) {
+                return '100 moves made';
+            } else if (x < 1 || x > 10 || y < 1 || y > 10) {
+                alert('enter values between 1 and 10');
+                return this.takeTurn();
+            } else {
+                this.moves.push([x, y]);
+                return [x, y];
+            }
         }
     }
 }
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,1 +1,25 @@
-import Player from '../player';
+import {Player} from '../player';
+
+test('computer player does not return values greater than 10', () => {
+    const player = new Player('computer');
+    for (let i = 0; i < 100; i++) {
+        let arr = player.takeTurn();
+        expect(arr[0]).toBeGreaterThan(0);
+        expect(arr[0]).toBeLessThanOrEqual(10);
+        expect(arr[1]).toBeGreaterThan(0);
+        expect(arr[1]).toBeLessThanOrEqual(10);
+    }
+    expect(player.takeTurn()).toBe('100 moves made');
+});
+
+test("player can't enter values greater than 10", () => {
+    const player2 = new Player('human');
+    for (let i = 0; i < 100; i++) {
+        let arr = player2.takeTurn();
+        expect(arr[0]).toBeGreaterThan(0);
+        expect(arr[0]).toBeLessThanOrEqual(10);
+        expect(arr[1]).toBeGreaterThan(0);
+        expect(arr[1]).toBeLessThanOrEqual(10);
+    }
+    expect(player2.takeTurn()).toBe('100 moves made');
+});
