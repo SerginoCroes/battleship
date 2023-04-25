8001937beccb7fe62cb59a8f8403ad2e945a8718
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-04-25 15:52:06 +0200

    progress

--- src/dom.js
+++ src/dom.js
@@ -8,4 +8,30 @@ export function buildBoard(board) {
         box.classList.add(`box${i}`);
         board.appendChild(box);
     }
+    buildLegend(board);
+}
+
+function buildLegend(board) {
+    const xcoords = board.querySelector('.xcoords');
+    const ycoords = board.querySelector('.ycoords');
+
+     for (let i = 1; i <= 10; i++) {
+        const xcoord = document.createElement('div');
+        xcoord.classList.add('coord');
+        const xtext = document.createElement('p');
+        xtext.textContent = `${i}`;
+        xcoord.appendChild(xtext);
+        xcoords.appendChild(xcoord);
+
+        const ycoord = document.createElement('div');
+        ycoord.classList.add('coord');
+        const ytext = document.createElement('p');
+        ytext.textContent = String.fromCharCode(64+i);
+        ycoord.appendChild(ytext);
+        ycoords.appendChild(ycoord);
+    } 
+}
+
+export function addShip(x, y, length) {
+    
 }
